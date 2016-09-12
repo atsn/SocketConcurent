526ea1ecf5d37e5e4736c3e65fdc9a57e5086b70
ander <ander@ANDERS-ACER>
2016-09-12 14:42:28 +0200

    Add project files.

---
+++ ServerTestMedNiko/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTestMedNiko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
